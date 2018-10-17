f589b36eb7a6389aaaf86726e6b72fe2821df347
earvincgats <43256796+earvincgats@users.noreply.github.com>
2018-10-17 13:37:32 +0800

    Update sale_type_membership.py

--- sale_type_membership/models/sale_type_membership.py
+++ sale_type_membership/models/sale_type_membership.py
@@ -7,16 +7,6 @@ import logging
 _logger = logging.getLogger(__name__)
 
 
-class SaleMembershipResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    allow_salesperson = fields.Many2many(string="Allow Salespersons", comodel_name="res.users")
-    
-    @api.onchange('user_id')
-    def onchange_allow_salesperson(self):
-        if self.user_id:
-            self.allow_salesperson = self.user_id
-
 class SaleMembershipProductTemplate(models.Model):
     _inherit = 'product.template'
 
